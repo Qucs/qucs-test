506697e735b633f59cda2929e77b719e3a5dbc47
Guilherme Brondani Torri <guitorri@gmail.com>
2013-12-02 18:32:51 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,189 @@
+# Qucs testing scripts.
+
+
+Objective: test the frond and back ends.
+
+
+
+## Running the test suite
+
+```
+$python run.py --prefix [qucs/installed/prefix]
+```
+
+## Output
+
+[Output without errors](output_normal.txt)
+[Output with errors](output_errors.txt)
+
+
+## How it works
+
+It considers every directory under `testsuite` as a project to be run. Then,
+
+ * It run tests to check the schematic to netlist conversion, using the Qucs-GUI code.
+
+ * It compares (diff) a reference netlist with the generated netlist on the step above.
+ 
+ * A simulation is run for each project
+ 
+ * Results are for the dependent variables are compared with reference results
+ 
+ * It collects : failed netlist diffs, simulation runtime, failed variable comparison, output and warnings (not used currently).  
+
+ 
+---
+
+## Running Qucs on the command line:
+
+Running Qucs and Qucsator from the command line makes it easy to automated testing.
+
+### Schematic to Netlist:
+
+It is possible to use Qucs GUI from the command line to read a schematic and create the netlist.
+
+```
+$qucs -n -i file.sch -o netlist.txt
+```
+
+### Netlist to Results:
+
+Having a netlist file it is possible to run the Qucsator backed to read the netlist and create the results (and log messages).
+
+```
+$qucsator -i netlist.txt -o result.dat
+```
+
+## Notes:
+
+ * Tests should not take too much time to run.
+ * Tests should focus into a particular feature which can be tested in isolation
+ * Tests could be categorised, ex. DC, AC analysis, a component...
+ * Tests should exercise corners that are crucial for stable releases.
+ * Before releasing, the test set should be run with no regressions.
+ * Git can be used to version test result (timing history?)
+
+
+# Test-case
+
+A test case is stored on a directory, similar to a Qucs project
+
+Suggested contents:
+
+ * test.txt
+   * test config file
+ * Schematic (.sch)
+   * Test schematic (name convention)
+   * Subcircuits dependencies
+ * Netlist (.txt)
+   * reference netlist
+ * Result (.dat)
+   * reference results
+ * Optional
+   * reference log file (.txt)
+   * PDF dump of schematic/data display
+   
+_What about asco, verilog, vhdl?_
+
+# Run a test-case
+
+The test-case is run depending on the available inputs. The main steps are listed below
+
+ * convert schematic into test netlist
+ * compare reference netlist with test netlist
+ * run netlist (or test netlist) store test result
+ * compare reference result with test result
+
+```
+Schematic ==> Netlist
+
+input:  [name].sch
+output: netlist_test.txt
+
+Netlist ==> Result
+
+input:  netlist.txt | netlist_test.txt
+output: result.dat | result_test.txt
+        log.txt
+        [asco.log]
+```
+
+```
+compare: 
+  - netlist X netlist_test
+  - result  X result_test
+```
+
+## Compare netlist
+
+Simple diff, skip first line. Retun different lines.
+
+## Check netlist
+
+Qucsator has the option to check a netlist. ?? what for ??
+
+## Compare results
+
+It uses the numpy [`allclose`](http://docs.scipy.org/doc/numpy/reference/generated/numpy.allclose.html) function to check how close together are all the dependent variables (arrays) on the reference and test data files.
+
+# Test Suite:
+
+Tests should be similar to projects. So making it easy to open on Qucs an use as examples.
+
+## Set structure:
+
+```
+qucs-test/
+  run.py
+
+  testsuite
+    TR_collpits_prj/
+      collpits.sch
+
+
+    AC_bandpass_prj/
+      bandpass.sch
+      opa.sch
+      bandpass.dat
+      netlist.txt
+      log.txt
+
+    ASCO_AC_fobar_prj/
+
+    VERILOG_
+
+    VHDL_
+```
+
+Notes:
+
+ * Need to identify the test schematic among the subcircuits 
+   * directory name, and the name of the schematic to be netlisted/simulated.
+   
+Whish-list
+   
+ * what about verilog, vhdl, asco?
+ * pack/unpack tarballs/examples?
+ 
+
+# Helper Scripts
+
+* Create/update list of test/prj:
+  - check consistency of the test-suite.
+  - it looks what tests are available.
+  - it adds new tests as they are found.
+  - it preserves the ones that are commented out #, give warning
+
+
+* Test behavior depends on the provided input/reference files
+  - if [sch] provided -> netlist, check
+  - if [sch, dat] provided -> nelist, [check], simulate, check
+  - if [sch, dat, log] provided -> do netlist, [check], simulate, check, statistics
+
+* Give option to create reference files when missing.
+
+* Table Report pass, fail, missing resource
+* Catch errors, time out, kill process if taking more than expected
+* Report checks, time improvement/regression
+
+* Html report?
---
+++ run.py
@@ -0,0 +1,313 @@
+
+
+import os
+import subprocess
+import difflib
+import pprint
+import sys
+import time
+import numpy as np
+
+import parse_result as parse
+
+
+# Add colors to the print messages
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+
+# Avoid color codes if output piped to file
+def pb(message):
+    if sys.stdout.isatty():
+        return bcolors.OKBLUE + message + bcolors.ENDC
+    else:
+        return message
+
+def pg(message):
+    if sys.stdout.isatty():
+        return bcolors.OKGREEN + message + bcolors.ENDC
+    else:
+        return message
+
+def pr(message):
+    if sys.stdout.isatty():
+        return bcolors.FAIL + message + bcolors.ENDC
+    else:
+        return message
+
+
+
+
+def get_subdirs(dir):
+    '''
+    Return a list of names of subdirectories.
+    input : dir to look up for subdirs.
+    '''
+    return [name for name in os.listdir(dir)
+            if os.path.isdir(os.path.join(dir, name))]
+
+
+def check_netlist(ref_netlist, output_netlist, skip=1, verbose=0):
+    '''
+    Does a diff with reference netlist and a test netlist.
+
+    input  : reference netlist
+    input  : test output netlist
+    output : diff status, different lines
+    '''
+    # http://docs.python.org/2.7/library/difflib.html
+    net_equal = True
+    bad_lines = []
+    with open(ref_netlist) as f, open(output_netlist) as g:
+
+        flines = f.readlines()
+        glines = g.readlines()
+
+        d = difflib.Differ()
+
+        # skip first line?
+        diff = list(d.compare(flines[skip:], glines[skip:]))
+
+        # show complete diff?
+        if verbose:
+            print ' '.join(diff)
+
+        for line in diff:
+            # look in diff for code [+ - ?]: unique to f, unique to g or not present in f or g
+            chars = set('+-?')
+            if any((c in chars for c in line[:1])):
+                net_equal = False
+                #print line
+                bad_lines.append(line)
+        return net_equal, bad_lines
+
+
+def run_schematic_to_netlist(proj, net_report={}):
+    '''
+    Run Qucs to pererform the schematic -> netlist conversion.
+
+    input  : test directory
+    input  : dictionary for the testsuite
+    output : dictionary reporting/updating issues
+    '''
+
+    name = proj.split('_')[-2]
+    #print name
+
+    tests_dir = os.getcwd()
+    #print tests_dir
+
+    proj_dir = os.path.join(tests_dir, 'testsuite', proj)
+    print '\nProject : ', proj_dir
+
+    # step into project
+    os.chdir(proj_dir)
+
+    input_sch = name+".sch"
+
+    # have schematic to convert
+    if os.path.isfile(input_sch):
+
+
+        output_netlist = "test_"+name+"_netlist.txt"
+
+        cmd = [prefix + "qucs", "-n", "-i", input_sch, "-o", output_netlist]
+        print 'Running : ', ' '.join(cmd)
+
+        p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        retval = p.wait()
+        # report issues if any
+        if retval:
+            print 'Return code: ', retval
+            for line in p.stdout.readlines():
+                print line,
+
+        ###
+        # netlist check
+        # - if reference netlist provided, diff compare with test netlist
+        # - report if missing
+        ###
+        ref_netlist = 'netlist.txt'
+        if os.path.isfile(ref_netlist):
+            print 'Comparing : diff %s %s' %(ref_netlist, output_netlist)
+            net_equal, bad_lines = check_netlist(ref_netlist, output_netlist)
+
+            if net_equal:
+                print pg('Diff     : PASS')
+            else:
+                print pr('Diff     : FAIL')
+
+            # collect things that are different
+            if not net_equal:
+                net_report[proj] = bad_lines
+        else:
+            net_report[proj] = 'missing reference [netlist.txt]'
+
+        # step out
+        os.chdir(tests_dir)
+
+    return net_report
+
+
+def run_simulation(proj, sim_report={}, prefix=''):
+    '''
+    Run simulation from reference netlist and compare outputs (dat, log)
+    '''
+
+    name = proj.split('_')[-2]
+
+    tests_dir = os.getcwd()
+
+    proj_dir = os.path.join(tests_dir, 'testsuite', proj)
+    print '\nProject : ', proj_dir
+
+    # cd into project
+    os.chdir(proj_dir)
+
+    input_net = "netlist.txt"
+
+    if os.path.isfile(input_net):
+
+        ref_dataset = name+".dat"
+        output_dataset = "test_"+name+".dat"
+
+        cmd = [prefix + "qucsator", "-i", input_net, "-o", output_dataset]
+        print 'Running : ', ' '.join(cmd)
+
+        # TODO run a few times, record average, add to report
+        tic = time.clock()
+        p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = p.communicate()
+        toc = time.clock()
+
+        runtime = toc - tic
+
+        print pb('Runtime: %f' %runtime)
+
+        # TODO use the outputs for something...
+        #print out
+        #print err
+
+        if not os.path.isfile(ref_dataset):
+            os.exit(pr('bad, missing ref output'))
+        if not os.path.isfile(output_dataset):
+            os.exit(pr('bad, missing test output'))
+
+        #all fine, lets compare results
+
+        print pb('load data %s' %(ref_dataset))
+        ref_data = parse.parse_file(ref_dataset)
+
+        print pb('load data %s' %(output_dataset))
+        test_data = parse.parse_file(output_dataset)
+
+        #print ref_data['variables']
+
+        print pb('Comparing dependent variables')
+
+        failed=[]
+        for name, kind in ref_data['variables'].items():
+            if kind == 'dep':
+                #print name
+                ref_trace  = ref_data[name]
+                test_trace = test_data[name]
+
+                if not np.allclose(ref_trace, test_trace):
+                    print pr('  Failed %s' %(name))
+                    failed.append(name)
+                else:
+                    print pg('  Passed %s' %(name))
+
+        # keep failed comparison
+        if failed:
+            sim_report[proj] = failed
+
+        # TODO add timestamp into qucsator. Or time the call.
+        # qucs creates the log.txt with time start and time end.
+
+        # step out
+        os.chdir(tests_dir)
+
+    return sim_report
+
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Process some integers.')
+
+    parser.add_argument('--prefix',type=str,
+                       help='prefix of installed Qucs (default: /usr/local/)')
+
+    args = parser.parse_args()
+    #print(args)
+
+    if args.prefix:
+        prefix = os.path.join(args.prefix, 'bin/')
+    else:
+        prefix = os.path.join('/usr/local/bin/')
+
+    if os.path.isfile(os.path.join(prefix, 'qucs')):
+        print pb('Found Qucs in: %s' %(prefix))
+    else:
+        sys.exit(pr('Oh dear, Qucs not found in: %s' %(prefix)))
+
+
+    print '\n'
+    print pb('******************************************')
+    print pb('** Test suite - Projects to be run      **')
+    print pb('******************************************')
+    testsuite = get_subdirs('./testsuite/')
+    pprint.pprint(testsuite)
+
+
+    print '\n'
+    print pb('******************************************')
+    print pb('** Test schematic to netlist conversion **')
+    print pb('******************************************')
+
+    # loop over testsuite
+    # messages are added to the dict, project as key
+    net_report = {}
+    for test in testsuite:
+        net_report =  run_schematic_to_netlist(test, net_report)
+
+
+    print '\n'
+    print pb('############################################')
+    print pb('#  Report schematic to netlist conversion  #')
+    if net_report.keys():
+        print pr('--> Found differences (!)')
+        pprint.pprint(net_report)
+    else:
+        print pg('--> No differences found.')
+
+
+    print '\n'
+    print pb('********************************')
+    print pb('** Test simulation and output **')
+    print pb('********************************')
+
+    # loop over testsuite
+    # messages are added to the dict, project as key
+    sim_report = {}
+    for test in testsuite:
+        sim_report = run_simulation(test, sim_report, prefix)
+
+    print '\n'
+    print pb('############################################')
+    print pb('#  Report simulation result comparison     #')
+    if sim_report.keys():
+        print pr('--> Found numerical differences (!)')
+        pprint.pprint(sim_report)
+    else:
+        print pg('--> No significant numerical differences found.')
+
+
+    print '\n'
+    print pb('###############  Done ######################')
+
