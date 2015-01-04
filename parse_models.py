ab3290e62d313325f7a6ed8cf3c1c0d880ce5007
Guilherme Brondani Torri <guitorri@gmail.com>
2015-01-04 19:58:07 +0100

    Update and fixes to `parse_model` script

--- parse_models.py
+++ parse_models.py
@@ -2,10 +2,11 @@
 '''
     Hacked parser to gather info about available components from Qucs C++ sources
 
-
     Note:
     * there a few name classes mismatches or inexistent beween qucs / qucs-core
       * Basic_BJT - ?
+    * it skips some HDL based components
+
 '''
 
 import glob
@@ -20,12 +21,28 @@ def get_class(s):
     return s[s.find("(")+1:s.find(")")]
 
 def scan_repo(repo):
+    '''
+    Scan the repository for:
+     - registered components
+     - component object properties
+
+    input:
+     - repo -- path to repository
+
+    return:
+     - report -- formatted text table
+     - data   -- dictionary with table data
+                 data[registered_Name]=[model, name, source file, category]
+
+    '''
 
 
-    # get class names and type of model
-    # ex. Resistor - Lumped, Diode - Nonlinear
+    # Get class names and type of model
+    # ex. Resistor - Lumped
+    #     Diode    - Nonlinear
 
-    mod = '/Users/guilherme/git/qucs/qucs/qucs/module.cpp'
+    mod = os.path.join(repo, 'qucs/qucs/module.cpp')
+    print 'Scan registred components: \t %s' %(mod)
 
     regs = [
     '  REGISTER_LUMPED',
@@ -36,7 +53,11 @@ def scan_repo(repo):
     '  REGISTER_VERILOGA',
     '  REGISTER_DIGITAL',
     '  REGISTER_FILE',
-    '  REGISTER_SIMULATION' ]
+    '  REGISTER_SIMULATION',
+    '  REGISTER_EXTERNAL'
+    ]
+    # Skip '  REGISTER_PAINT'
+
 
     mod_reg = {}
     for index in range(len(regs)):
@@ -52,16 +73,28 @@ def scan_repo(repo):
 
                     mod_reg[model_class] = kind
 
+
+    # Gather component `data` indo dictionary, saved as Picke later on.
+    #  data[registered_Name]=[model, name, source file, category]
     data = {}
 
-    # list all sources
-    devices = glob.glob(repo+'*.cpp')
+    comp_src = 'qucs/qucs/components/'
+    comp_dir = os.path.join(repo, comp_src)
+
+    print 'Looking for components dir: \t %s%s' %(comp_dir, '*.cpp')
+    if not os.path.isdir(comp_dir):
+        sys.exit("Directory not found: <components>"+comp_src)
+
+
+    # list all component sources
+    devices = glob.glob(comp_dir+'*.cpp')
 
     report  = ''
     report += '\n%20s' %('-'*80)
     report += '\n%-20s | %-20s | %-15s | %-10s' %('Registered Class', 'Model Name', 'Instance Prefix', 'Source')
     report += '\n%20s' %('-'*80)
 
+    # Parse data in component cpp files
     for src in devices:
         constr = False
         nam = False
@@ -70,9 +103,17 @@ def scan_repo(repo):
         with open(src) as fp:
 
             # skip non-device files
-            skip = ['component.cpp', 'componentdialog.cpp', 'optimizedialog.cpp',
-            'spicedialog.cpp', 'vafile.cpp', 'verilogfile.cpp', 'vhdlfile.cpp', 'vfile.cpp']
+            skip = ['component.cpp',
+                    'componentdialog.cpp',
+                    'optimizedialog.cpp',
+                    'spicedialog.cpp',
+                    'vafile.cpp',
+                    'vacomponent.cpp',
+                    'verilogfile.cpp',
+                    'vhdlfile.cpp',
+                    'vfile.cpp']
 
+            # component cpp file
             base = os.path.basename(src)
             if base in skip:
                 continue # skip to next src
@@ -86,6 +127,7 @@ def scan_repo(repo):
 
             for line in fp:
                 if not constr:
+                    # look for constructor
                     if '::' in line:
                         constr = True
                         # class name
@@ -135,8 +177,11 @@ def scan_repo(repo):
 
 
         # try to skip unknown classes from Qucs into Qucs-core
+        # ignore unregistered
         if regName in mod_reg:
             data[regName]=[model, name, base, mod_reg[regName]]
+        else:
+            print '  Not registered class: ', regName
 
     report += '\n%20s' %('-'*80)
     report += '\nTotal: %i' %(len(data.keys()))
@@ -155,27 +200,25 @@ if __name__ == "__main__":
 
     repo = str(sys.argv[1])
 
-    comp_src = '/qucs/qucs/components/'
-    comp_dir = repo + comp_src
 
-    print 'Looking for: ', comp_dir
-    if not os.path.isdir(comp_dir):
-        sys.exit("Directory not found: <repo>"+comp_src)
+    print ''
+    print 'Looking for repository: \t %s' %(repo)
+    if not os.path.isdir(repo):
+        sys.exit("Directory not found: <repo>"+repo)
 
-    # Parse data
-    print 'Parsing sources in: ', comp_dir
-    report, data = scan_repo(comp_dir)
+    # Scan code for registered components and their properties
+    report, data = scan_repo(repo)
 
     # Save table
     report_out = 'qucs_components.txt'
-    print 'Saving table: ', report_out
+    print 'Saving components table: \t', report_out
     with open(report_out, 'w') as myFile:
         myFile.write(report)
 
     # Save data, to be used on the test reports
     # Save a dictionary into a pickle file.
     datafile = 'qucs_components_data.p'
-    print 'Saving Python data: ', datafile
+    print 'Saving data (as Pickle): \t', datafile
     pickle.dump( data, open( datafile, "wb" ) )
 
     # parse type from registered
