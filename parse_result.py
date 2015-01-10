ee01c5ad17e657782d9aaca5788698705c2104ef
Guilherme Brondani Torri <guitorri@gmail.com>
2015-01-10 22:41:53 +0100

    parser: add logic to read matrix indep
    * Sort of a hack. Look for better implementation

--- parse_result.py
+++ parse_result.py
@@ -2,6 +2,12 @@ import re
 import numpy as np
 
 
+'''
+The matrix part is a bit of hack!! Sorry
+'''
+
+import collections
+
 def parse_file(name):
 
     file = open(name)
@@ -13,9 +19,18 @@ def parse_file(name):
     shape = []
     variables = {}
 
+    mName = '' # matrix name, use as state
+
+    # load matrix as dict of dict
+    matrix = collections.defaultdict(dict)
+
+
     for line in file:
         #print line
         if line.startswith('<'):
+
+            # a matrix or vector in equations are stored as indep
+
             if line.startswith('<indep'):
                 #print line
                 r = re.match(r'\<(\w+) (\S+) (\d+)\>', line)
@@ -24,13 +39,35 @@ def parse_file(name):
                 # the total number of points
                 numpoints = numpoints * int(g[2])
                 name = g[1]
-                # reserve an array for the values
-                data[name] = np.zeros(int(g[2]))
-                ind = 0
-                # save the simulation points in an array
-                shape = np.append(shape, int(g[2]))
-                # save that this variable is independent
-                variables[name] = 'indep'
+
+                # try to get name and indices
+                #print name
+                if '[' in name:
+                    # got a matrix,
+                    # need to read it till the end to find out size
+                    m = re.split(',|\[|\]', name)
+
+                    # set matrix
+                    mName = m[0]
+                    row = m[1]
+                    col = m[2]
+
+                    # temp dict
+                    if mName not in matrix.keys():
+                        matrix[mName]['val'] = []
+
+                    variables[mName] = 'indep'
+                else:
+                    mName='' # unset matrix
+
+                    # reserve an array for the values
+                    # independent can also be complex
+                    data[name] = np.zeros(int(g[2]), np.complex64)
+                    ind = 0
+                    # save the simulation points in an array
+                    shape = np.append(shape, int(g[2]))
+                    # save that this variable is independent
+                    variables[name] = 'indep'
 
             if line.startswith('<dep'):
                 #print line
@@ -49,6 +86,10 @@ def parse_file(name):
             if(jind == -1):
                 # real number -> just parse it
                 val = float(line)
+
+                if mName:
+                    matrix[mName]['type'] = 'float' #kind val
+
             else:
                 # complex number -> break into re/im part
                 val_re = line[0:jind-1]
@@ -57,10 +98,20 @@ def parse_file(name):
                 # and convert it into a complex number
                 val = complex(float(val_re), float(val_im))
 
-            # store the extracted datapoint
-            data[name][ind] = val
-            ind = ind + 1
+                if mName:
+                    matrix[mName]['type'] = 'complex' #kind val
 
+            # store the extracted datapoint
+            if mName:
+                # matrix... do nothing?
+                #print mName, row, col, val
+                matrix[mName]['row'] = row
+                matrix[mName]['col'] = col
+                matrix[mName]['val'].append(val)
+                #print matrix
+            else:
+                data[name][ind] = val
+                ind = ind + 1
 
     data['variables'] = variables
     # reverse the shape variable in order to get the reshape operation (see below)
@@ -77,4 +128,12 @@ def parse_file(name):
             temp_data = data[key]
             data[key] = np.reshape(temp_data, shape)
 
+    for key in matrix.keys():
+        #print matrix[key]
+        val = matrix[key]['val']
+        row = int(matrix[key]['row'])
+        col = int(matrix[key]['col'])
+        data[key] = np.reshape(val, (row,col))
+
     return data
+
