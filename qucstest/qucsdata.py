203837bb0e0274ab5571e71670b6d96a53303431
Guilherme Brondani Torri <guitorri@gmail.com>
2015-02-13 21:05:21 +0100

    Replace Qucs Dataset parser

--- qucstest/qucsdata.py
+++ qucstest/qucsdata.py
@@ -1,142 +1,110 @@
 """
-Module to handle the qucsdata result file.
+Module to handle a Qucs Dataset result file.
 """
 
 import re
 import numpy as np
+import pprint as pp
 
-'''
-The matrix part is a bit of hack!! Sorry
-'''
+class QucsData:
+    """
+    Read an store a result file writen by Qucsator.
+    """
+    def __init__(self, filename):
+        # source dataset
+        self.filename = filename
+        # list of all variable names
+        self.names = []
+        # data[name] = array
+        self.data = {}
+        # data dependent[name]=list(names)
+        self.dependent = {}
 
-import collections
+        # parse
+        self.read(self.filename)
 
-def parse_file(name):
+    def __str__(self):
+        """
+        Print self.
+        """
+        pr  = ''
+        pr += 'File name: %s\n' %self.filename
+        pr += 'names:'
+        for name in self.names:
+            pr += '    \'%s\': shape %s\n' %(name, self.data[name].shape)
+        pr += 'dependent: \n'
+        pr += pp.pformat(self.dependent, indent=4)
+        pr += '\n'
+        return pr
 
-    file = open(name)
+    def read(self,fileName):
+        """
+        Qucs Dataset parser.
+        """
+        variable = ''
+        with open(fileName) as qucsdata:
+            for line in qucsdata:
+                # header
+                r = re.match(r'\<Qucs Dataset (\d+\.\d+\.\d+)\>', line)
+                if r:
+                    version = r.group(1)
 
-    # the dict this function returns
-    data = {}
-    numpoints = 1
-    ind = 0
-    shape = []
-    variables = {}
+                if '<indep' in line:
+                    # independent variables
+                    r = re.match(r'(\S+) (\S+) (\d+)', line.translate(None,"<>"))
+                    kind, identifier, size =  r.groups()
+                    # collect identifiers
+                    self.names.append(identifier)
+                    variable = identifier
+                    continue # next line
 
-    mName = '' # matrix name, use as state
+                if '<dep' in line:
+                    # dependent variables
+                    # one or more independent, y=f(v,w,...)
+                    r = re.findall('(\S+)', line.translate(None,"<>"))
+                    identifier = r[1]
+                    self.dependent[identifier]= r[2:]
+                    self.names.append(identifier)
+                    variable = identifier
+                    continue # next line
 
-    # load matrix as dict of dict
-    matrix = collections.defaultdict(dict)
+                if '</indep' in line:
+                    # reached end of independent variable
+                    # cast from list to numpy array
+                    values = np.array(self.data[variable])
+                    self.data[variable] = values
+                    variable = ''
+                    continue #  next line
+
+                if '</dep' in line:
+                    # reached end of dependent variable
+                    # dependencies are stacked on a single column
+                    # cast from list to 2D array
+                    # TODO check for multidimensional ndim > 2
+                    # <dep Variable axis1 axis2 axis3 ...>
+                    shape = []
+                    for dep in self.dependent[variable]:
+                        size = len(self.data[dep])
+                        shape.append (size)
+                    values = np.array(self.data[variable])
+                    self.data[variable] = values.reshape(tuple(shape),order='F')
+                    variable = ''
+                    continue #  next line
+
+                if variable:
+                    # collect data from line into temporary list
+                    if variable not in self.data.keys():
+                        self.data[variable] = []
+                    value = line.strip()
+                    if ('j' in value):
+                        # put imaginary to the end
+                        value = value.replace("j","")
+                        value = value+'j'
+                        value = complex(value)
+                    else:
+                        value = float(value)
+                    self.data[variable].append(value)
 
 
-    for line in file:
-        #print line
-        if line.startswith('<'):
 
-            # a matrix or vector in equations are stored as indep
-
-            if line.startswith('<indep'):
-                #print line
-                r = re.match(r'\<(\w+) (\S+) (\d+)\>', line)
-                g = r.groups()
-                # there can be several independent variables -> numpoints keeps
-                # the total number of points
-                numpoints = numpoints * int(g[2])
-                name = g[1]
-
-                # try to get name and indices
-                #print name
-                if '[' in name:
-                    # got a matrix,
-                    # need to read it till the end to find out size
-                    m = re.split(',|\[|\]', name)
-
-                    # set matrix
-                    mName = m[0]
-                    row = m[1]
-                    col = m[2]
-
-                    # temp dict
-                    if mName not in matrix.keys():
-                        matrix[mName]['val'] = []
-
-                    variables[mName] = 'indep'
-                else:
-                    mName='' # unset matrix
-
-                    # reserve an array for the values
-                    # independent can also be complex
-                    data[name] = np.zeros(int(g[2]), np.complex64)
-                    ind = 0
-                    # save the simulation points in an array
-                    shape = np.append(shape, int(g[2]))
-                    # save that this variable is independent
-                    variables[name] = 'indep'
-
-            if line.startswith('<dep'):
-                #print line
-                r = re.match(r'\<dep (\S+)', line)
-                g = r.groups()
-                name = g[0]
-                # reserve a complex matrix to be on the safe side
-                data[name] = np.zeros(int(numpoints), np.complex64)
-                ind = 0
-                # store that this is a dependent variable
-                variables[name] = 'dep'
-
-        else:
-            jind = line.find('j')
-
-            if(jind == -1):
-                # real number -> just parse it
-                val = float(line)
-
-                if mName:
-                    matrix[mName]['type'] = 'float' #kind val
-
-            else:
-                # complex number -> break into re/im part
-                val_re = line[0:jind-1]
-                sign = line[jind-1]
-                val_im = sign + line[jind+1:-1]
-                # and convert it into a complex number
-                val = complex(float(val_re), float(val_im))
-
-                if mName:
-                    matrix[mName]['type'] = 'complex' #kind val
-
-            # store the extracted datapoint
-            if mName:
-                # matrix... do nothing?
-                #print mName, row, col, val
-                matrix[mName]['row'] = row
-                matrix[mName]['col'] = col
-                matrix[mName]['val'].append(val)
-                #print matrix
-            else:
-                data[name][ind] = val
-                ind = ind + 1
-
-    data['variables'] = variables
-    # reverse the shape variable in order to get the reshape operation (see below)
-    # correct
-    shape = shape[::-1]
-
-    # here comes the clever trick :-)
-    # if a dependent variable depends on N > 1 (independent) variables,
-    # we reshape the vector we have obtained so far into an N-dimensional
-    # matrix
-    for key in data['variables']:
-        temp = data['variables'][key]
-        if temp == 'dep':
-            temp_data = data[key]
-            data[key] = np.reshape(temp_data, shape)
-
-    for key in matrix.keys():
-        #print matrix[key]
-        val = matrix[key]['val']
-        row = int(matrix[key]['row'])
-        col = int(matrix[key]['col'])
-        data[key] = np.reshape(val, (row,col))
-
-    return data
 
