891b1ca05a765e94f7403ccdec322e58d5ab744f
andresmmera <andresmmera@protonmail.com>
2019-07-24 07:22:25 +0200

    Modify testsuite/RFMW/Oscillators/TR_colpitts_prj
    The data display option was set not to open automatically. It is added a
    graph to show the output waveform in the time domain. Additionally, it
    was wroten an Octave script to show the spectrum using the fft.

---
+++ testsuite/RFMW/Oscillators/TR_Colpitts_prj/Colpitts.m
@@ -0,0 +1,49 @@
+close all
+clc
+data = loadQucsDataSet('Colpitts.dat');
+RF = getQucsVariable(data, 'Output.Vt');
+time = getQucsVariable(data, 'time');
+R_LOAD = getQucsVariable(data, 'RL');
+T = time(2)-time(1);
+fs = 1/T;
+N = length(time);
+
+%Prepare freq axis
+f_axis = linspace(-fs/2, fs/2, N);
+
+
+
+RF_spectrum = fftshift(fft(RF))/N;
+
+%Remove the left side of the spectrum
+RF_spectrum = RF_spectrum(ceil(N/2):end);%Voltage spectrum
+
+%Calculate power spectrum
+P_spectrum = abs(RF_spectrum).^2/R_LOAD;
+
+%Convert the power spectrum to dBm
+P_spectrum_dBm = 10*log10(P_spectrum) + 30;
+
+
+f_axis = f_axis(ceil(N/2):end)*1e-3;%kHz
+
+%Plot voltage spectrum
+plot(f_axis, P_spectrum_dBm, 'o-b')
+grid on;
+% Plot settings
+f1 = 5e2;
+f2 = 1.5e3;
+ymin = -60;
+ymax = -5;
+
+axis([f1, f2, ymin, ymax])
+set(gca, 'xtick', f1:1e2:f2)
+set(gca, 'ytick', ymin:5:ymax)
+legend('Pout')
+title('Colpitts oscillator')
+ylabel('Pout (dBm)')
+xlabel('freq (kHz)')
+
+%Get the fundamental frequency
+[val, f_index] = max(abs(RF_spectrum));
+display(sprintf('f_0 %1.3g kHz', f_axis(f_index)))
