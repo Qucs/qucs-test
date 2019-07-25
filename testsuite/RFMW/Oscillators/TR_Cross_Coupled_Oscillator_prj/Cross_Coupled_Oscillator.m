e8070bf9111ba77738472e82f8ae28203e5bb6e2
andresmmera <andresmmera@protonmail.com>
2019-07-25 06:56:34 +0200

    Modify testsuite/RFMW/Oscillators/TR_lc_osc_prj
    The data display option was set not to open automatically. The project
    was renamed as TR_Cross_Coupled_Oscillator_prj.  It was added an Octave
    script to plot the spectrum. Using the series-to-parallel
    transformation, the resistor was put in parallel with the L-C since it’s
    more common to find that in the literature.

---
+++ testsuite/RFMW/Oscillators/TR_Cross_Coupled_Oscillator_prj/Cross_Coupled_Oscillator.m
@@ -0,0 +1,49 @@
+close all
+clc
+data = loadQucsDataSet('Cross_Coupled_Oscillator.dat');
+RF = getQucsVariable(data, 'Output.Vt');
+time = getQucsVariable(data, 'time');
+R_LOAD = getQucsVariable(data, 'RL');
+T = time(2)-time(1);
+fs = 1/T;
+N = length(time);
+
+%Prepare freq axis
+f_axis = linspace(-fs/2, fs/2, N);
+
+
+
+RF_spectrum = fftshift(fft(RF))/N;
+
+%Remove the left side of the spectrum
+RF_spectrum = RF_spectrum(ceil(N/2):end);%Voltage spectrum
+
+%Calculate power spectrum
+P_spectrum = abs(RF_spectrum).^2/R_LOAD;
+
+%Convert the power spectrum to dBm
+P_spectrum_dBm = 10*log10(P_spectrum) + 30;
+
+
+f_axis = f_axis(ceil(N/2):end)*1e-6;%MHz
+
+%Plot voltage spectrum
+plot(f_axis, P_spectrum_dBm, 'o-b')
+grid on;
+% Plot settings
+f1 = 1000;
+f2 = 5000;
+ymin = -40;
+ymax = 10;
+
+axis([f1, f2, ymin, ymax])
+set(gca, 'xtick', f1:500:f2)
+set(gca, 'ytick', ymin:5:ymax)
+legend('Pout')
+title('Symmetric cross-coupled oscillator')
+ylabel('Pout (dBm)')
+xlabel('freq (MHz)')
+
+%Get the fundamental frequency
+[val, f_index] = max(abs(RF_spectrum));
+display(sprintf('f_0 %1.3g MHz', f_axis(f_index)))
