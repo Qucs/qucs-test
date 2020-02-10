d33588c1096008c923dd3d6fb429376be34bb3f5
oxmon-2500 <blachuta@gmx.net>
2020-02-10 23:04:56 +0100

    run_tests.sh modified

--- run_tests.sh
+++ run_tests.sh
@@ -4,7 +4,7 @@
 
 NO_CORES=$(nproc --all)
 if [ -z "${QUCS_HOME}" ]; then
-  QUCS_HOME=~/qucs
+  QUCS_HOME=~/local/qucs
 fi
 if [ -z "${QUCS_HOME2}" ]; then
   QUCS_HOME2=~/tmp/qucs
@@ -15,10 +15,15 @@ if [ ! -d "${QUCS_HOME}" ]; then
     exit -1
 fi
 
-python3 run.py --prefix ${QUCS_HOME}/bin/ --exclude skip.txt --qucs --reset -mp ${NO_CORES}
-#python3 run.py --prefix ${QUCS_HOME}/bin/ --exclude skip.txt --qucsator --reset -mp ${NO_CORES}
+#python3 run.py --prefix ${QUCS_HOME}/bin/ --exclude skip.txt --qucs -mp ${NO_CORES}
+python3 run.py --prefix ${QUCS_HOME}/bin/ --exclude skip.txt --qucsator -mp ${NO_CORES}
 #python3 run.py --prefix ${QUCS_HOME}/bin/ --exclude skip.txt -p all #print all
 #python3 run.py --prefix ${QUCS_HOME}/bin/ --compare ${QUCS_HOME}/bin ${QUCS_HOME2}/bin
 #python3 run.py --prefix ${QUCS_HOME}/bin/ --exclude skip.txt --qucs --plot-interactive
 
 #python3 run_equations.py --prefix ${QUCS_HOME}/bin/
+
+# see .gitignore
+#find . -type f -name "test_*" -exec rm {} \;
+#find . -type f -name "*.pyc" -exec rm {} \;
+
